991b4ab13c8a41649fe2d171b5cc1c911ccfc4a0
Meetabeast <84871625+Meetabeast@users.noreply.github.com>
2022-04-04 11:43:44 +0200

    Add files via upload

---
+++ commands/lookup.js
@@ -0,0 +1,60 @@
+const { default: axios } = require("axios");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "lookup",
+    description: "Look up a user with the id",
+    options: [
+        {
+            name: "id",
+            description: "Enter the User ID here",
+            type: 3,
+            required: true
+        }
+    ],
+    async execute(interaction) {
+        
+
+        const user_ID = interaction.options.getString("id");
+
+        if(!user_ID) {
+            return;
+        }
+
+        axios.get(`https://discord.com/api/users/${user_ID}`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bot OTU5NTc0NDcxMTU5NzIxOTk0.Ykd3XQ.Nl-1nIJH4L5lOyBlECQ2q8KQdbo`
+            }
+        }).then(res => {
+            
+
+            const embed = new MessageEmbed()
+            embed.setTitle(`User Look Up [${res.data.username}#${res.data.discriminator}]`)
+            embed.setColor(res.data.banner_color ? res.data.accent_color : 'RANDOM')
+            embed.setThumbnail(`https://cdn.discordapp.com/avatars/${user_ID}/${res.data.avatar}`)
+            embed.addField(`ID:`, `${res.data.id}`)
+            embed.addField(`Username:`, `${res.data.username}`)
+            embed.addField(`Bot:`, `${res.data.bot ? true : false}`)
+
+            let receive = '';
+
+            receive = res.data.banner;
+
+            if(receive !== null) {
+                let format = 'png';
+                if(receive.substring(0, 2) === 'a_') {
+                    format = 'gif';
+                }
+
+                const banner = `https://cdn.discordapp.com/banners/${user_ID}/${receive}.${format}?=size=1028` || null;
+                
+                embed.setImage(banner || null)
+            }
+
+            interaction.reply({embeds: [embed]});
+        }).catch(() => {
+            return interaction.reply({content: "I don't find informations"})
+        })
+    }
+}
---
+++ index.js
@@ -0,0 +1,56 @@
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+const fs = require("fs");
+const config = require("./config.json");
+
+const cmd = new Map();
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+client.on("ready", () => {
+    console.log("User id lookup is online");
+    client.user.setActivity({name: 'I Search for Users', type: "PLAYING"});
+});
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    cmd.set(command.name, command);
+}
+
+const rest = new REST({ version: '9' }).setToken(config.client_token);
+
+const data = Array.from(cmd, ([name, value]) => value).map((c) => ({
+    name: c.name,
+    description: c.description,
+    options: c.options,
+}));
+
+(async () => {
+    try {
+        await rest.put(
+            Routes.applicationCommands("959574471159721994"),
+            {body: data}
+        )
+        
+    } catch (error) {
+        console.log("Error", error)
+    }
+})();
+
+
+client.on("interactionCreate", async (interaction) => {
+    if(interaction.isCommand()) {
+        
+        const command = cmd.get(interaction.commandName);
+        if(command) {
+            command.execute(interaction, client);
+        }
+    }
+});
+
+client.login(config.client_token)
